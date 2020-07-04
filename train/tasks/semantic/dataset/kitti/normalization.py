b9d655c4aac769e93cac99d69f515430e6566479
Ali Abdelkader <aly.abdelkader@aucegypt.edu>
2020-07-04 13:49:11 +0200

    normailzation script - fix bug in filtering points - refactor code and add comments

--- train/tasks/semantic/dataset/kitti/normalization.py
+++ train/tasks/semantic/dataset/kitti/normalization.py
@@ -11,8 +11,8 @@ import cv2
 import torch
 from tqdm import tqdm
 import shutil
-MIN_W_ANGLE = np.deg2rad(-40)
-MAX_W_ANGLE = np.deg2rad(40)
+# min_w_angle = np.deg2rad(-40)
+# max_w_angle = np.deg2rad(40)
 MAX_POINTS = 150000
 
 class LaserScan:
@@ -30,6 +30,7 @@ class LaserScan:
     self.reset()
     self.image_width = image_width
     self.image_height = image_height
+    self.label = None
 
   def reset(self):
     """ Reset scan members. """
@@ -100,8 +101,8 @@ class LaserScan:
     remissions = scan[:, 3]  # get remission
     #image = Image.open(imagename)
     image = cv2.imread(imagename)
-    image = cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
-    self.set_points(points, remissions, image)
+    self.image = cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
+    self.set_points(points, remissions, self.image)
 
   def set_points(self, points, remissions=None, image=None):
     """ Set scan attributes (instead of opening from file)
@@ -169,7 +170,7 @@ class LaserScan:
         
       # set rgb value according to image
       # result[idx, :] = np.array(image.getpixel((int(x),int(y)))).astype(np.float32) # CHANGED FROM float64
-      result[idx, :] = np.array(image[x, y]).astype(np.float32) 
+      result[idx, :] = image[y, x].astype(np.float32) 
     return result
 
   def do_range_projection(self):
@@ -206,6 +207,9 @@ class LaserScan:
     rgb = self.get_rgb(transformed_velo_to_image.T[self.image_filter_condition, :], self.image)
     self.rgb = rgb
 
+    transformed_velo_to_image = transformed_velo_to_image.T[self.image_filter_condition, :]
+
+
     # get depth of all points
     depth = np.linalg.norm(self.points, 2, axis=1)
     self.depth = depth
@@ -221,7 +225,7 @@ class LaserScan:
 
     # get projections in image coords
     #proj_x = 0.5 * (yaw / np.pi + 1.0)          # in [0.0, 1.0]
-    proj_x = (yaw - MIN_W_ANGLE) / (MAX_W_ANGLE - MIN_W_ANGLE) # in [0.0, 1.0]
+    proj_x = (yaw - self.min_w_angle) / (self.max_w_angle - self.min_w_angle) # in [0.0, 1.0]
     proj_y = 1.0 - (pitch + abs(fov_down)) / fov        # in [0.0, 1.0]
 
     # scale to image size using angular resolution
@@ -260,6 +264,7 @@ class LaserScan:
     self.proj_remission[proj_y, proj_x] = remission
     self.proj_idx[proj_y, proj_x] = indices
     self.proj_mask = (self.proj_idx > 0).astype(np.int32) # CHANGED FROM float32.
+    # self.proj_rgb[proj_y, proj_x] = rgb
     self.proj_rgb[proj_y, proj_x] = rgb
 
   def open_label(self, filename, filter=True):
@@ -324,25 +329,47 @@ if __name__== "__main__":
         help='should save projected points',
     )
     parser.add_argument(
-        '--save_projected',
+        '--save_filtered_scan',
         type=bool,
         required=False,
         default=False,
-        help='should save projected points',
+        help='should save scan after filtering by image fov',
     )
     parser.add_argument(
-        '--save_projected_dir',
+        '--save_filtered_dir',
         type=str,
         required=False,
         default=".",
-        help='directory to save projected points in',
+        help='directory to save filtered scan in',
+    )
+    parser.add_argument(
+        '--include_sets',
+        type=str,
+        required=True,
+        default="train",
+        help='which sets to include in pgm stats calculation, train,valid,test',
+    )
+    parser.add_argument(
+        '--min_w_angle',
+        type=str,
+        required=True,
+        default="-40",
+        help='min yaw angle in degrees in LiDAR FOV to consider while constructing pgm',
+    )
+    parser.add_argument(
+        '--max_w_angle',
+        type=str,
+        required=True,
+        default="40",
+        help='max yaw angle in degrees in LiDAR FOV to consider while constructing pgm',
     )
     FLAGS, unparsed = parser.parse_known_args()
 
     # open data config file
     try:
         print("Opening data config file %s" % FLAGS.data_cfg)
-        DATA = yaml.safe_load(open(FLAGS.data_cfg, 'r'))
+        DATA = yaml.load(open(FLAGS.data_cfg, 'r'))
+        
     except Exception as e:
         print(e)
         print("Error opening data yaml file.")
@@ -359,15 +386,31 @@ if __name__== "__main__":
 
     root = os.path.join(FLAGS.dataset, "sequences")
     print(FLAGS)
-    if FLAGS.save_projected:
-      save_root = os.path.join(FLAGS.save_projected_dir, "sequences")
+    if FLAGS.save_filtered_dir:
+      save_root = os.path.join(FLAGS.save_filtered_dir, "sequences")
       if not os.path.isdir(save_root):
         os.makedirs(save_root, exist_ok=True)
 
+    
+    LaserScan.min_w_angle = np.deg2rad(float(FLAGS.min_w_angle))
+    LaserScan.max_w_angle = np.deg2rad(float(FLAGS.max_w_angle))
+    
     train_sequences=DATA["split"]["train"]
     valid_sequences=DATA["split"]["valid"]
-    sequences = train_sequences
-    sequences.extend(valid_sequences)
+    test_sequences=DATA["split"]["test"]
+    sequences = train_sequences.copy()
+    sequences.extend(valid_sequences.copy())
+    sequences.extend(test_sequences.copy())
+    
+    include_sets = FLAGS.include_sets.split(",")
+    print(f"sets included {include_sets}")
+    include_seq_in_stats = []
+    if "train" in include_sets:
+      include_seq_in_stats.extend(train_sequences.copy())
+    if "valid" in include_sets:
+      include_seq_in_stats.extend(valid_sequences.copy())
+    if "test" in include_sets:
+      include_seq_in_stats.extend(test_sequences.copy())
 
     sensor = ARCH["dataset"]["sensor"]
     sensor_img_H = sensor["img_prop"]["height"]
@@ -379,7 +422,7 @@ if __name__== "__main__":
     total_scan_files = []
     total_image_files = []
 
-    all_pgm = []
+    all_pgms = []
 
     # fill in with names, checking that all sequences are complete
     for seq in sequences:
@@ -399,7 +442,7 @@ if __name__== "__main__":
         scan_path = os.path.join(root, seq, "velodyne")
         image_path = os.path.join(root, seq, "image_2")
         label_path = os.path.join(root, seq, "labels")
-        if FLAGS.save_projected:
+        if FLAGS.save_filtered_scan:
           save_scan_path = os.path.join(save_root, seq, "velodyne")
           save_image_path = os.path.join(save_root, seq, "image_2")
           save_label_path = os.path.join(save_root, seq, "labels")
@@ -427,75 +470,108 @@ if __name__== "__main__":
         image_files.sort()
         label_files.sort()
 
-        for scan_file, image_file, label_file in tqdm(list(zip(scan_files, image_files, label_files)), "processing seq {}".format(seq)):
-            # image_size = Image.open(image_file).size
-            image_size = cv2.imread(image_file).shape
-            scan = LaserScan(project=True,
+        def save_data(scan: LaserScan, scan_file: str, image_file: str, label_file: str = None):
+          """
+          function saves the scan points after they were filtered by image field of view
+          along with the image, filtered labels
+          Args:
+            scan: LaserScan object to be save
+            scan_file: path to the original scan file before filtering
+            image_file: path to the original image file before filtering
+            lable_file: path to the original label file
+          """
+          path_split = lambda p: os.path.normpath(p).split(os.sep)
+          save_scan_file = os.path.join(save_scan_path, path_split(scan_file)[-1] )
+          save_image_file = os.path.join(save_image_path, path_split(image_file)[-1])
+          
+          # x,y,z,remission,range,r,g,b
+          save_scan = np.concatenate([scan.points, scan.remissions.reshape(-1,1), scan.depth.reshape(-1,1), scan.rgb], axis=1)
+          save_scan = save_scan.astype(np.float32)
+          assert save_scan.shape[0] == scan.points.shape[0]
+          save_scan.tofile(save_scan_file)
+          # np.save(save_scan_file, save_scan)
+          shutil.copyfile(image_file, save_image_file)
+
+          if scan.label is not None:
+            save_label_file = os.path.join(save_label_path, path_split(label_file)[-1])
+            save_label = scan.label.astype(np.int32)
+            assert save_scan.shape[0] == scan.label.shape[0]
+            save_label.tofile(save_label_file)
+            # np.save(save_label_file, save_label)
+
+        def get_pgm(scan: LaserScan):    
+          """
+          function returns polar grid map from scan object
+          Args:
+            scan: LaserScan object
+          
+          Returns:
+            polar grid map, shape: (8, pgm_height, pgm_width), channels: range,x,y,z,remission,r,g,b
+          """
+          # make a tensor of the uncompressed data (with the max num points)
+          unproj_n_points = scan.points.shape[0]
+  
+          # get points and labels
+          proj_range = torch.from_numpy(scan.proj_range).clone()
+          proj_xyz = torch.from_numpy(scan.proj_xyz).clone()
+          proj_rgb = torch.from_numpy(scan.proj_rgb).clone()
+          proj_remission = torch.from_numpy(scan.proj_remission).clone()
+          proj_mask = torch.from_numpy(scan.proj_mask)
+        
+          proj = torch.cat([proj_range.unsqueeze(0).clone(),
+                    proj_xyz.clone().permute(2, 0, 1),
+                    proj_remission.unsqueeze(0).clone(),
+                    proj_rgb.clone().permute(2, 0, 1)])
+          proj = proj * proj_mask.float()
+          return proj
+        
+        def get_scan(scan_file: str, image_file: str, label_file: str = None):
+          """
+          function creates LaserScan object from scan_file
+          Args:
+            scan_file: path to original scan file
+            image_file: path to rgb image file
+          
+          Returns:
+            LaserScan object
+
+          """
+          image_size = cv2.imread(image_file).shape
+          scan = LaserScan(project=True,
                     H=sensor_img_H,
                     W=sensor_img_W,
                     fov_up=sensor_fov_up,
                     fov_down=sensor_fov_down,
                     calib_params=odometry_manager.calib,
-                    image_height=image_size[1],
-                    image_width=image_size[0])
-            
-            scan.open_scan(scan_file, image_file)
+                    image_height=image_size[0],
+                    image_width=image_size[1])
+          scan.open_scan(scan_file, image_file)
+          if label_file is not None:
             scan.open_label(label_file)
-            #cv2.imwrite("pgm.png", scan.proj_rgb.astype(np.uint8))
+          return scan
+     
+      
+        if len(label_files) > 0:
+          generator = tqdm(list(zip(scan_files, image_files, label_files)), "processing seq {}".format(seq))
+        else:
+          generator = tqdm(list(zip(scan_files, image_files)), "processing seq {}".format(seq))
+        
+        for scan_paths in generator:
+          scan = get_scan(*scan_paths)  
+          if FLAGS.save_filtered_scan:         
+            save_data(scan, *scan_paths)
 
-            ############# save projected ##################################
-            if FLAGS.save_projected:
-              def path_split(p):
-                norm = os.path.normpath(p)
-                split = norm.split(os.sep)
-                return split
-              save_scan_file = os.path.join(save_scan_path, path_split(scan_file)[-1] )
-              save_image_file = os.path.join(save_image_path, path_split(image_file)[-1])
-              save_label_file = os.path.join(save_label_path, path_split(label_file)[-1])
-              # x,y,z,remission,range,r,g,b
-              #save_scan = np.stack([scan.points, scan.remissions, scan.depth, scan.rgb], axis=0)
-              save_scan = np.concatenate([scan.points, scan.remissions.reshape(-1,1), scan.depth.reshape(-1,1), scan.rgb], axis=1)
-              save_scan = save_scan.astype(np.float32)
-              save_label = scan.label.astype(np.int32)
-              assert save_scan.shape[0] == scan.points.shape[0]
-              assert save_scan.shape[0] == scan.label.shape[0]
-
-              save_scan.tofile(save_scan_file)
-              save_label.tofile(save_label_file)
-              shutil.copyfile(image_file, save_image_file)
-            ############# save projected ##################################
-
-              
-            # make a tensor of the uncompressed data (with the max num points)
-            unproj_n_points = scan.points.shape[0]
+          if int(seq) in include_seq_in_stats:
+            proj = get_pgm(scan)
+            all_pgms.append(proj)
     
-            # get points and labels
-            proj_range = torch.from_numpy(scan.proj_range).clone()
-            proj_xyz = torch.from_numpy(scan.proj_xyz).clone()
-            proj_rgb = torch.from_numpy(scan.proj_rgb).clone()
-            proj_remission = torch.from_numpy(scan.proj_remission).clone()
-            proj_mask = torch.from_numpy(scan.proj_mask)
-           
-            proj_x = torch.full([MAX_POINTS], -1, dtype=torch.long)
-            proj_x[:unproj_n_points] = torch.from_numpy(scan.proj_x)
-            proj_y = torch.full([MAX_POINTS], -1, dtype=torch.long)
-            proj_y[:unproj_n_points] = torch.from_numpy(scan.proj_y)
-            proj = torch.cat([proj_range.unsqueeze(0).clone(),
-                      proj_xyz.clone().permute(2, 0, 1),
-                      proj_remission.unsqueeze(0).clone()])
-            proj = torch.cat((proj, proj_rgb.clone().permute(2, 0, 1)), 0)
-            proj = proj * proj_mask.float()
+    if len(all_pgms) > 0:
+      stacked_pgms = torch.stack(all_pgms)
+      print("Stacked Size: ", stacked_pgms.size())
 
-            all_pgm.append(proj)
-
-    stacked_pgms = torch.stack(all_pgm)
-    print("Stacked Size: ", stacked_pgms.size())
-
-    means = torch.mean(stacked_pgms, dim=[0,2,3]).cpu().detach().numpy()
-    stds = torch.std(stacked_pgms, dim=[0,2,3]).cpu().detach().numpy()
-    print("Means: ", means)
-    print("Stds: ", stds)
-    np.savetxt("means.csv", means, delimiter=",")
-    np.savetxt("stds.csv", stds, delimiter=",")
-    # print("Means: ", means)
-    # print("Stds: ", stds)
+      means = torch.mean(stacked_pgms, dim=[0,2,3]).cpu().detach().numpy()
+      stds = torch.std(stacked_pgms, dim=[0,2,3]).cpu().detach().numpy()
+      print("Means: ", means)
+      print("Stds: ", stds)
+      np.savetxt("pgm_means.csv", means, delimiter=",")
+      np.savetxt("pgm_stds.csv", stds, delimiter=",")
